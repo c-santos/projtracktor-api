6fda14504de61c85ffdd873fafaa60c1cc4b7c05
c-santos <jcms121899@gmail.com>
2024-12-07 12:12:51 +0800

    updated setup script

--- setup-local.sh
+++ setup-local.sh
@@ -5,16 +5,13 @@ set -x
 
 D="docker"
 DC="docker-compose"
-$DC down
+$DC down --remove-orphans
 
 # Spin up conatiners
-$DC build --no-cache
-$DC run --rm my-template npm install
 $DC up -d
 
 # Run migration and seeders
-$DC exec my-template npm run typeorm:migration:generate --name=Init
-$DC exec my-template npm run typeorm:migration:run
+$DC run projtracktor npm run typeorm:migration:run
 # $DC exec my-template npm run typeorm:seed
 
 # show logs
