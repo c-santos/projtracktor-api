d0dee3c2f5040b085e0b51ab3c7d78dd91a93d25
c-santos <jcms121899@gmail.com>
2024-12-15 01:47:15 +0800

    preferred single tag table for project and tasks

--- src/infrastructure/database/models/project-tag.model.ts
+++ src/infrastructure/database/models/project-tag.model.ts
@@ -1,19 +1,20 @@
-import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 import { BaseModel } from './base.model';
-import { Tag } from './tag.model';
 import { Project } from './project.model';
+import { Tag } from './tag.model';
 
 @Entity('project_tags')
 export class ProjectTag extends BaseModel {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column(() => Tag)
+    tag: Tag;
+
     @PrimaryColumn({ name: 'project_id' })
     @ManyToOne(() => Project, (project) => project.id, {
         cascade: true,
     })
     @JoinColumn({ name: 'project_id', referencedColumnName: 'id' })
     projectId: string;
-
-    @PrimaryColumn({ name: 'tag_id' })
-    @ManyToOne(() => Tag, (tag) => tag.id)
-    @JoinColumn({ name: 'tag_id', referencedColumnName: 'id' })
-    tagId: string;
 }
--- src/infrastructure/database/models/tag.model.ts
+++ src/infrastructure/database/models/tag.model.ts
@@ -1,22 +1,9 @@
-import { BaseModel } from './base.model';
-import { Column, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { Entity } from 'typeorm';
-import { TaskTag } from './task-tag.model';
-import { ProjectTag } from './project-tag.model';
+import { Column } from 'typeorm';
 
-@Entity('tag')
-export class Tag extends BaseModel {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-
-    @Column({ type: 'text' })
+export class Tag {
+    @Column()
     name: string;
 
-    @OneToMany(() => TaskTag, (taskTag) => taskTag.tagId, { nullable: true })
-    tasks: TaskTag[];
-
-    @OneToMany(() => ProjectTag, (projTag) => projTag.projectId, {
-        nullable: true,
-    })
-    projects: ProjectTag[];
+    @Column({ nullable: true })
+    color: string;
 }
