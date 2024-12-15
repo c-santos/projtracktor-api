829b851d32301681739e3cb3e903a30572c3047b
c-santos <jcms121899@gmail.com>
2024-12-15 13:28:40 +0800

    updated setup script

--- setup-local.sh
+++ setup-local.sh
@@ -11,7 +11,7 @@ $DC down --remove-orphans
 $DC up -d
 
 # Run migration and seeders
-$DC run projtracktor npm run typeorm:migration:run
+$DC exec projtracktor npm run typeorm:migration:run
 # $DC exec my-template npm run typeorm:seed
 
 # show logs
